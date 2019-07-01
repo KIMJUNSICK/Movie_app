465fcb2c73fbed4f6be4258eeb1b9944d8a7daea
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-02 05:26:03 +0900

    Data Flow with Props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React from "react";
 import "./App.css";
 import Movie from "./Movie ";
 
+const movieTitles = [
+  "Inglorius Bastards",
+  "Matrix",
+  "Christmas of August",
+  "Oldboy"
+];
+
+const movieImg = [
+  "https://upload.wikimedia.org/wikipedia/ko/4/4f/%EB%B0%94%EC%8A%A4%ED%84%B0%EC%A6%88_%EA%B1%B0%EC%B9%9C_%EB%85%80%EC%84%9D%EB%93%A4.jpg",
+  "https://cdn.images.express.co.uk/img/dynamic/36/590x/MATRIX-761666.jpg",
+  "https://t1.daumcdn.net/cfile/2679604F525F7D4B20",
+  "https://lefthandhorror.files.wordpress.com/2013/10/oldboy.jpg?w=620"
+];
+
 function App() {
   return (
     <div className="App">
-      <Movie />
+      <Movie title={movieTitles[0]} poster={movieImg[0]} />
+      <Movie title={movieTitles[1]} poster={movieImg[1]} />
+      <Movie title={movieTitles[2]} poster={movieImg[2]} />
+      <Movie title={movieTitles[3]} poster={movieImg[3]} />
     </div>
   );
 }
--- src/Movie .js
+++ src/Movie .js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Movie.css";
 
-function Movie() {
-  return (
-    <div>
-      <MoviePoster />
-      <h1>hello. this is a movie</h1>;
-    </div>
-  );
+class Movie extends Component {
+  render() {
+    return (
+      <div>
+        <MoviePoster poster={this.props.poster} />
+        <h1>{this.props.title}</h1>
+      </div>
+    );
+  }
 }
 
-function MoviePoster() {
-  return <img src="https://t1.daumcdn.net/cfile/2679604F525F7D4B20" />;
+class MoviePoster extends Component {
+  render() {
+    console.log(this.props);
+    return <img src={this.props.poster} />;
+  }
 }
 
 export default Movie;
