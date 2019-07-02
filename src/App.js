bcc9f67e853f51e97764d5a15df69f87d2ae03a4
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-02 16:08:24 +0900

    Loading States

--- src/App.js
+++ src/App.js
@@ -6,53 +6,52 @@ class App extends Component {
   // Render : componentWillMount( ) > render( ) > componentDidMount( )
   // Update : componentWillReciveProps( ) > shouldComponentUpdate( ) > componentWillUpdate( ) > render( ) > componentDidUpdate( )
 
-  state = {
-    movies: [
-      {
-        title: "Inglorius Bastards",
-        poster:
-          "https://www.torrentmap.com/data/parsing_img/movie_old/0_B19DPQFb_bf5230d211dd3641378dd4f9095924ec6d2dc5a2.jpg"
-      },
-      {
-        title: "Matrix",
-        poster:
-          "https://cdn.images.express.co.uk/img/dynamic/36/590x/MATRIX-761666.jpg"
-      },
-      {
-        title: "Christmas of August",
-        poster: "https://t1.daumcdn.net/cfile/2679604F525F7D4B20"
-      },
-      {
-        title: "Oldboy",
-        poster:
-          "https://lefthandhorror.files.wordpress.com/2013/10/oldboy.jpg?w=620"
-      }
-    ]
-  };
+  state = {};
 
   componentDidMount() {
     setTimeout(() => {
       this.setState({
         movies: [
           {
+            title: "Inglorius Bastards",
+            poster:
+              "https://www.torrentmap.com/data/parsing_img/movie_old/0_B19DPQFb_bf5230d211dd3641378dd4f9095924ec6d2dc5a2.jpg"
+          },
+          {
+            title: "Matrix",
+            poster:
+              "https://cdn.images.express.co.uk/img/dynamic/36/590x/MATRIX-761666.jpg"
+          },
+          {
+            title: "Christmas of August",
+            poster: "https://t1.daumcdn.net/cfile/2679604F525F7D4B20"
+          },
+          {
+            title: "Oldboy",
+            poster:
+              "https://lefthandhorror.files.wordpress.com/2013/10/oldboy.jpg?w=620"
+          },
+          {
             title: "Trainspotting",
             poster:
               "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiyTBtrgBlI9WDc4s8OmR2jimz8U-XXAzM1TxtC9J0yqP7QSbU"
-          },
-          ...this.state.movies
+          }
         ]
       });
     }, 5000);
   }
 
+  _renderingMoives = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    });
+    return movies;
+  };
+
   render() {
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return (
-            <Movie title={movie.title} poster={movie.poster} key={index} />
-          );
-        })}
+        {this.state.movies ? this._renderingMoives() : "Loading"}
       </div>
     );
   }
