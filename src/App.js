7ee255b7356b546d7b9069c1644eed340e5c45fe
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-02 15:48:51 +0900

    Practicing this.setState( )
    Practice for adding Data on Time Order

--- src/App.js
+++ src/App.js
@@ -2,40 +2,45 @@ import React, { Component } from "react";
 import "./App.css";
 import Movie from "./Movie ";
 
-const movies = [
-  {
-    title: "Inglorius Bastards",
-    poster:
-      "https://www.torrentmap.com/data/parsing_img/movie_old/0_B19DPQFb_bf5230d211dd3641378dd4f9095924ec6d2dc5a2.jpg"
-  },
-  {
-    title: "Matrix",
-    poster:
-      "https://cdn.images.express.co.uk/img/dynamic/36/590x/MATRIX-761666.jpg"
-  },
-  {
-    title: "Christmas of August",
-    poster: "https://t1.daumcdn.net/cfile/2679604F525F7D4B20"
-  },
-  {
-    title: "Oldboy",
-    poster:
-      "https://lefthandhorror.files.wordpress.com/2013/10/oldboy.jpg?w=620"
-  }
-];
-
 class App extends Component {
   // Render : componentWillMount( ) > render( ) > componentDidMount( )
   // Update : componentWillReciveProps( ) > shouldComponentUpdate( ) > componentWillUpdate( ) > render( ) > componentDidUpdate( )
 
   state = {
-    greeting: "hello"
+    movies: [
+      {
+        title: "Inglorius Bastards",
+        poster:
+          "https://www.torrentmap.com/data/parsing_img/movie_old/0_B19DPQFb_bf5230d211dd3641378dd4f9095924ec6d2dc5a2.jpg"
+      },
+      {
+        title: "Matrix",
+        poster:
+          "https://cdn.images.express.co.uk/img/dynamic/36/590x/MATRIX-761666.jpg"
+      },
+      {
+        title: "Christmas of August",
+        poster: "https://t1.daumcdn.net/cfile/2679604F525F7D4B20"
+      },
+      {
+        title: "Oldboy",
+        poster:
+          "https://lefthandhorror.files.wordpress.com/2013/10/oldboy.jpg?w=620"
+      }
+    ]
   };
 
   componentDidMount() {
     setTimeout(() => {
       this.setState({
-        greeting: "sooooory"
+        movies: [
+          {
+            title: "Trainspotting",
+            poster:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiyTBtrgBlI9WDc4s8OmR2jimz8U-XXAzM1TxtC9J0yqP7QSbU"
+          },
+          ...this.state.movies
+        ]
       });
     }, 5000);
   }
@@ -43,8 +48,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return (
             <Movie title={movie.title} poster={movie.poster} key={index} />
           );
