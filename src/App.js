b1f53d7e11d3d923ac4d2e5b05149dd9357cd370
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-03 10:22:34 +0900

    Get Movie Data with AJAX
    Get Movie Data from YTS.LT with Promise and async/await

--- src/App.js
+++ src/App.js
@@ -9,43 +9,35 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Inglorius Bastards",
-            poster:
-              "https://www.torrentmap.com/data/parsing_img/movie_old/0_B19DPQFb_bf5230d211dd3641378dd4f9095924ec6d2dc5a2.jpg"
-          },
-          {
-            title: "Matrix",
-            poster:
-              "https://cdn.images.express.co.uk/img/dynamic/36/590x/MATRIX-761666.jpg"
-          },
-          {
-            title: "Christmas of August",
-            poster: "https://t1.daumcdn.net/cfile/2679604F525F7D4B20"
-          },
-          {
-            title: "Oldboy",
-            poster:
-              "https://lefthandhorror.files.wordpress.com/2013/10/oldboy.jpg?w=620"
-          },
-          {
-            title: "Trainspotting",
-            poster:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiyTBtrgBlI9WDc4s8OmR2jimz8U-XXAzM1TxtC9J0yqP7QSbU"
-          }
-        ]
-      });
-    }, 5000);
+    this._getMovies();
   }
 
   _renderingMoives = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    const movies = this.state.movies.map(movie => {
+      return (
+        <Movie
+          title={movie.title}
+          poster={movie.medium_cover_image}
+          key={movie.id}
+        />
+      );
     });
     return movies;
+  };
+
+  _getMovies = async () => {
+    const movies = await this._callAPI();
+    console.log(movies);
+    this.setState({
+      movies
+    });
+  };
+
+  _callAPI = () => {
+    return fetch("https://yts.lt/api/v2/list_movies.json?sort_by=rating")
+      .then(potato => potato.json())
+      .then(tomato => tomato.data.movies)
+      .catch(error => console.log(error));
   };
 
   render() {
