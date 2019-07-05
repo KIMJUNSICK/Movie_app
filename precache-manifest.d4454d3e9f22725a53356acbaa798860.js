1e6b42b6cc09a53e0625d12b3204b3fa1732c95b
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-05 19:13:09 +0900

    Updates

---
+++ precache-manifest.d4454d3e9f22725a53356acbaa798860.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1638107f052057f656b73688ccc553b6",
+    "url": "/Movie_app/index.html"
+  },
+  {
+    "revision": "04ad8bbd098fd3485642",
+    "url": "/Movie_app/static/css/main.e25c2a54.chunk.css"
+  },
+  {
+    "revision": "ad63ab08a2a8e5504836",
+    "url": "/Movie_app/static/js/2.7b834e7d.chunk.js"
+  },
+  {
+    "revision": "04ad8bbd098fd3485642",
+    "url": "/Movie_app/static/js/main.032feaea.chunk.js"
+  },
+  {
+    "revision": "bc3a21fec6a0d0ce35f7",
+    "url": "/Movie_app/static/js/runtime~main.bdf1a524.js"
+  }
+]);
